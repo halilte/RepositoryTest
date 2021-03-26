be92632d409f2e23ee01c71bd29f0e0b75166583
halilte <halilte@web.de>
2021-03-26 23:30:46 +0100

    bhhhhhhhh

--- MyGitProject/src/MayGitPackage/MyGitClass.java
+++ MyGitProject/src/MayGitPackage/MyGitClass.java
@@ -2,6 +2,20 @@ package MayGitPackage;
 
 public class MyGitClass {
 	public static void main(String[] args) {
-
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("baska birsey yaziyorum");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
+		System.out.println("deneme devam ediyor");
+		System.out.println("deneme devam ediyor 2");
 	}
 }
